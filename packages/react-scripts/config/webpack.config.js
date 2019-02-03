92f9db7da8b88cc1de9036fb6f1a7572de3f3616
Manu Phatak <manu@thinkcerca.com>
2019-02-03 13:51:29 -0600

    add graphql support

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -325,6 +325,11 @@ module.exports = function(webpackEnv) {
           include: paths.appSrc,
         },
         {
+          test: /\.(graphql|gql)$/,
+          include: paths.appSrc,
+          loader: require.resolve('graphql-tag/loader'),
+        },
+        {
           // "oneOf" will traverse all following loaders until one will
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
